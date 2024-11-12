950a64640f699934b580275d0db2cfc84dd92229
Slava Asipenko <slava.asipenko@gmail.com>
2024-11-11 21:19:37 -0500

    scaffolding cleanup

--- brave-puffin-thruster-efficiency-tester.ino
+++ brave-puffin-thruster-efficiency-tester.ino
@@ -1,1 +1,4 @@
 // blank to VSCode's Arduino extension happy. true main file is main.cpp
+//
+// Built for and tested on original ESP32
+// Won't work on later ESP32's due to the lack of Bluetooth serial (SPP) support 
