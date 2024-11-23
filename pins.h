c283362f628e15a0db019cedfaccd97c36c72ac2
Slava Asipenko <slava.asipenko@gmail.com>
2024-11-23 16:22:42 -0500

    SD card pins

--- pins.h
+++ pins.h
@@ -1,8 +1,23 @@
-#define PIN_MOTOR_PWM 12
+/*
+*** SD card breakout board
+CS to GPIO 5
+MOSI to GPIO 23
+MISO to GPIO 19
+SCK to GPIO 18
+5V for power; should have built-in logic level convertors to 3V
+*/
 
-// default Wire / I2C pins:
-// SDA 21
-// SCL 22
+#define PIN_SD_CS 5
+
+#define PIN_MOTOR_PWM 14
+
+// SPI_SCK 18
+// SPI_MISO 19
+
+// I2C SDA 21
+// I2C SCL 22
+
+// SPI MOSI 23
 
 #define PIN_STATUS_LED 25
 
