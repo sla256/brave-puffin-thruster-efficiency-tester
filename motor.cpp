9817d2eba0b9a0ddae062cc691de3c620d1a5949
Slava Asipenko <slava.asipenko@gmail.com>
2024-12-07 20:25:03 -0500

    throttle control bug fixe, adjusted PWM for 30A ESC

--- motor.cpp
+++ motor.cpp
@@ -3,12 +3,13 @@
 #include "pins.h"
 
 const int motorStopPulseWidth = 800;    // must start at this level to initialize ESC properly
-const int motorMinPulseWidth = 1820; // 1210;
-const int motorMaxPulseWidth = 2100; // 1600;
+const int motorMinPulseWidth = 1190;
+const int motorMaxPulseWidth = 1890;
 
 Servo motorControl;
 
 int currentMotorPulseWidth = motorStopPulseWidth;
+int currentMotorThrottleLevel = 0;
 
 void initMotor() {
     Serial.println("MOTOR");
@@ -19,18 +20,18 @@ void initMotor() {
 
 void stopMotor() {
     currentMotorPulseWidth = motorStopPulseWidth;
+    currentMotorThrottleLevel = 0;
     motorControl.write(currentMotorPulseWidth);
 }
 
 void setMotorThrottle(int throttleLevel) {
+    currentMotorThrottleLevel = throttleLevel;
     currentMotorPulseWidth = map(throttleLevel, 1, 100, motorMinPulseWidth, motorMaxPulseWidth);
     motorControl.write(currentMotorPulseWidth);
 }
 
 bool incrementMotorThrottle(int throttleLevelIncrement) {
-    if (currentMotorPulseWidth + throttleLevelIncrement > motorMaxPulseWidth) return false;
-
-    setMotorThrottle(currentMotorPulseWidth + throttleLevelIncrement);
+    setMotorThrottle(currentMotorThrottleLevel + throttleLevelIncrement);
     return true;
 }
 
--- test_controller.cpp
+++ test_controller.cpp
@@ -6,27 +6,36 @@
 #include "sensors.h"
 #include "test_controller.h"
 
-const int testStepDurationMs = 3*1000;
-char buffer2[50];
+const int testStepDurationMs = 5*1000;
+char buffer2[100];
 
 int16_t currentTestRun = 0; // to print for
 int currentTestStep = 0; // 1-100 during each test run
-int testStepToStopTestRunAt = 100;
+int stepToStopTestRunAt = 100;
 unsigned long currentTestStepStartTimeMs;
 
-float currentTestStepCumulativePowerValuesW; // all power values measured in the current test step, added up
-long currentTestStepCumulativeThrustForceValuesG; // all pull force values measured in the current test step, added up
-int numberOfMeasurementsInCurrentTestStep; // number of samples, to calculate average power value at the end of the step
+long currTestStepCumulativeCurrentValuesMa;
+long currTestStepCumulativeVoltageValuesMv;
+float currTestStepCumulativePowerValuesW; // all power values measured in the current test step, added up
+long currTestStepCumulativeThrustForceValuesG; // all pull force values measured in the current test step, added up
+int numOfMeasurementsInCurrTestStep; // number of samples, to calculate average power value at the end of the step
 
+int motorPulseWidthValuesForAllTestSteps[100];
+int averageCurrentValuesForAllTestSteps[100]; // in mA
+int averageVoltageValuesForAllTestSteps[100]; // in mV
 float averagePowerValuesForAllTestSteps[100]; // in watts
 int averageThrustForceValuesForAllTestSteps[100]; // in grams
+
+char* printFormatMeasuredData(int step);
 
 void progressToNextStep() {
     currentTestStep++;
     currentTestStepStartTimeMs = millis();
-    currentTestStepCumulativePowerValuesW = 0;
-    currentTestStepCumulativeThrustForceValuesG = 0;
-    numberOfMeasurementsInCurrentTestStep = 0;
+    currTestStepCumulativeCurrentValuesMa = 0;
+    currTestStepCumulativeVoltageValuesMv = 0;
+    currTestStepCumulativePowerValuesW = 0;
+    currTestStepCumulativeThrustForceValuesG = 0;
+    numOfMeasurementsInCurrTestStep = 0;
     
     // ramp up the motor but don't measure yet - will be done later in handleCurrentTest; throttle can go to 101 briefly
     setMotorThrottle(currentTestStep);
@@ -36,11 +45,12 @@ void beginNewEfficiencyTest(int testStepToStopAt=100) {
     currentTestRun = readInt16FromEeprom(EEPROM_TEST_RUN_NUMBER_ADDR) + 1;
     writeInt16ToEeprom(EEPROM_TEST_RUN_NUMBER_ADDR, currentTestRun);
 
-    testStepToStopTestRunAt = testStepToStopAt;
+    stepToStopTestRunAt = testStepToStopAt;
 
-    Serial.print(btPrintln("Beginning test run #"));
-    Serial.print(currentTestRun);
-    Serial.print(", stopping at ");
+    Serial.print("Beginning test run #");
+    Serial.println(currentTestRun);
+    btPrintln(currentTestRun);
+    Serial.print("Stopping at ");
     Serial.println(testStepToStopAt);
 
     currentTestStep = 0;
@@ -49,53 +59,69 @@ void beginNewEfficiencyTest(int testStepToStopAt=100) {
 
 void handleTestInProgress() {
     if (currentTestStep == 0) {
-        stopMotor();
         return;
     }
 
     if (millis() - currentTestStepStartTimeMs > testStepDurationMs) {
         // print latest values from the previous step
-        snprintf(buffer2, sizeof(buffer2), "%d: %.1f, %d", 
-            currentTestStep,
-            averagePowerValuesForAllTestSteps[currentTestStep - 1], 
-            averageThrustForceValuesForAllTestSteps[currentTestStep - 1]);
-        Serial.println(btPrintln(buffer2));
-
+        char* formattedData = printFormatMeasuredData(currentTestStep - 1);
+        Serial.println(btPrintln(formattedData));
         progressToNextStep();
         return;
     }
 
-    if (currentTestStep > testStepToStopTestRunAt) {
+    if (currentTestStep > stepToStopTestRunAt) {
         stopEfficiencyTest();
         return;
     }
 
     // still in the middle of an active test step, measure and record power & thrust force
-    float currentPowerW = getCurrentFromIna226() * getVoltageFromIna226() / 1000000;
-    currentTestStepCumulativePowerValuesW += currentPowerW;
-    currentTestStepCumulativeThrustForceValuesG += getPullForce();
-    numberOfMeasurementsInCurrentTestStep++;
+    int currentMa = getCurrentFromIna226();
+    int voltageMv =  getVoltageFromIna226();
+    float powerW = currentMa * voltageMv / 1000000;
 
-    averagePowerValuesForAllTestSteps[currentTestStep - 1] = 
-        currentTestStepCumulativePowerValuesW / numberOfMeasurementsInCurrentTestStep;
-    averageThrustForceValuesForAllTestSteps[currentTestStep - 1] = 
-        currentTestStepCumulativeThrustForceValuesG / numberOfMeasurementsInCurrentTestStep;
+    // calculate averages
+    currTestStepCumulativeCurrentValuesMa += currentMa;
+    currTestStepCumulativeVoltageValuesMv += voltageMv;
+    currTestStepCumulativePowerValuesW += powerW;
+    currTestStepCumulativeThrustForceValuesG += getPullForce();
+    numOfMeasurementsInCurrTestStep++;
+
+    int i = currentTestStep - 1;
+    averageCurrentValuesForAllTestSteps[i] = currTestStepCumulativeCurrentValuesMa / numOfMeasurementsInCurrTestStep;
+    averageVoltageValuesForAllTestSteps[i] = currTestStepCumulativeVoltageValuesMv / numOfMeasurementsInCurrTestStep;
+    averagePowerValuesForAllTestSteps[i] = currTestStepCumulativePowerValuesW / numOfMeasurementsInCurrTestStep;
+    averageThrustForceValuesForAllTestSteps[i] = currTestStepCumulativeThrustForceValuesG / numOfMeasurementsInCurrTestStep;
+    motorPulseWidthValuesForAllTestSteps[i] = getCurrentMotorPulseWidth();
 }
 
 void stopEfficiencyTest() {
     currentTestStep = 0;
     stopMotor();
 
-    Serial.print(btPrintln("Finished test run #"));
-    Serial.println(btPrintln(currentTestRun));
+    Serial.print("Finished test run #");
+    Serial.println(currentTestRun);
+    btPrintln(currentTestRun);
 
     char testResultsFileName[20];
     snprintf(testResultsFileName, sizeof(testResultsFileName), "/r%d_%d.csv", currentTestRun, millis() / 1000);
-    writeToFileOnSdCard(testResultsFileName, "Power in watts, force in grams");
+    writeToFileOnSdCard(testResultsFileName, "Step, Pulse width, Current mA, Voltage mV, Power watts, Force grams");
 
-    for(int i = 0; i < testStepToStopTestRunAt; i++) {
-        snprintf(buffer2, sizeof(buffer2), "%.1f, %d", averagePowerValuesForAllTestSteps[i], averageThrustForceValuesForAllTestSteps[i]);
-        writeToFileOnSdCard(testResultsFileName, buffer2);
-        Serial.println(btPrintln(buffer2));
+    for(int i = 0; i < stepToStopTestRunAt; i++) {
+        char* formattedData = printFormatMeasuredData(i);
+        writeToFileOnSdCard(testResultsFileName, formattedData);
+        Serial.println(btPrintln(formattedData));
     }
+}
+
+char* printFormatMeasuredData(int step) {
+    snprintf(buffer2, sizeof(buffer2), "%d, %d, %d, %d, %.1f, %d", 
+        step + 1,
+        motorPulseWidthValuesForAllTestSteps[step],
+        averageCurrentValuesForAllTestSteps[step],
+        averageVoltageValuesForAllTestSteps[step],
+        averagePowerValuesForAllTestSteps[step],
+        averageThrustForceValuesForAllTestSteps[step]);
+
+    return buffer2;
 }
